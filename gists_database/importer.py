c7209ce5d02228b92e3769327eec9aedcbf2fca1
selmabensaid <selmabensaid@hotmail.com>
2019-05-04 21:10:58 +0000

    added functionality to importer.py and search.py. passed all tests.

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,29 @@
 import requests
+from requests.exceptions import HTTPError
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    params={
+        'username':username
+    }
+    
+    response=requests.get('https://api.github.com/users/{}/gists'.format(username))
+    if not response.ok:
+        raise HTTPError()
+    gists=response.json()
+    query= 'Insert into gists (github_id, html_url, git_pull_url, git_push_url, commits_url, forks_url, public, created_at, updated_at, comments, comments_url) values (:github_id, :html_url, :git_pull_url, :git_push_url, :commits_url, :forks_url, :public, :created_at, :updated_at, :comments, :comments_url) '
+    if commit:
+        for gist in gists:
+            values={
+                'github_id':gist['id'],
+                'html_url': gist['html_url'],
+                'git_pull_url': gist['git_pull_url'],
+                'git_push_url': gist['git_push_url'],
+                'commits_url': gist['commits_url'],
+                'forks_url': gist['forks_url'],
+                'public': gist['public'],
+                'created_at': gist['created_at'],
+                'updated_at': gist['updated_at'],
+                'comments': gist['comments'],
+                'comments_url': gist['comments_url']
+            }
+            db.execute(query, values)
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,16 @@
-from .models import Gist
 
-def search_gists(db_connection, **kwargs):
-    pass
+from .models import Gist
+import sqlite3
+
+def search_gists(db_connection, github_id=None, created_at=None):
+    
+    if (github_id!= None and  created_at==None) :
+        cursor=db_connection.execute(('select * from gists where github_id=:github_id'),{'github_id':github_id})
+    elif (created_at!=None and github_id==None):
+        cursor=db_connection.execute(('select * from gists where datetime(created_at)== datetime(:created_at)'),{'created_at':created_at})
+    elif (github_id and created_at):
+        cursor=db_connection.execute(('select * from gists where github_id=:github_id and datetime(created_at)==datetime(:created_at)'),{'github_id':github_id, 'created_at':created_at })
+    else:
+        cursor=db_connection.execute(('select * from gists'))
+    gists_data= cursor.fetchall()
+    return [Gist(gist) for gist in gists_data]
